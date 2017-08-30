4c8f095daaecf0511bd6fa6606b213b8bd0c5808
amstergo <amstergo46@gmail.com>
2017-08-30 13:38:40 +0300

    cartman find butterfly

--- main.lua
+++ main.lua
@@ -1,34 +1,29 @@
+vector = require "vector"
+
+position = vector(10, 10)
+
+speed = vector(100, 200)
+
 function love.load()
   love.graphics.setBackgroundColor(8, 5, 94)
   cartman = love.graphics.newImage("cartman.png")
-  ponch = love.graphics.newImage("ponch.png")
-  tick = require "tick"
-
-  x = 100
-  y = 50
-
-  sp = { x = 300, y = 200 }
-
-  rand = { x = math.random(1, 600), y = math.random(1, 400) }
+  butterfly = love.graphics.newImage("butterfly.png")
+  font = love.graphics.newFont("verdana.ttf", 20)
 end
 
 function love.update(dt)
-  tick.update(dt)
+  mousePosition = vector(love.mouse.getX(), love.mouse.getY())
+  dir = mousePosition - position
+  ndir = dir:normalized()
 
-  if love.keyboard.isDown("right") then
-    x = x + sp.x * dt
-  elseif love.keyboard.isDown("left") then
-    x = x - sp.x * dt
-  end
-
-  if love.keyboard.isDown("down") then
-    y = y + sp.y * dt
-  elseif love.keyboard.isDown("up") then
-    y = y - sp.y * dt
+  if dir:len2() > 16 then
+    position = position + ndir * 200 * dt
   end
 end
 
 function love.draw()
-  love.graphics.draw(cartman, x, y)
-  love.graphics.draw(ponch, rand.x, rand.y)
+  love.graphics.setFont(font)
+  love.graphics.draw(cartman, position.x, position.y)
+  love.graphics.draw(butterfly, mousePosition.x, mousePosition.y)
+  love.graphics.print("text", position.x, position.y)
 end
